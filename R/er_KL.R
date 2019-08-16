1c50bea1daaf204e4d6133d3d0409061895fdd52
Anthony Ebert <anthonyebert@gmail.com>
2019-08-16 14:26:26 -0400

    Attempt to compute entropy without simulation.

--- R/er_KL.R
+++ R/er_KL.R
@@ -1,13 +1,17 @@
 
 #' @export
-er_KL <- function(p, nl = 10, pl = 0.1, replicates = 1000, ...){
+er_KL <- function(p, nl = 10, pl = 0.1, replicates = 1000, entropy_sim = TRUE, ...){
 
   ne <- choose(nl,2)
 
   y <- replicate(replicates, er_edges(nl, p, ...))
   y1 <- mean(y)
   y2 <- mean(log(choose(ne, y)))
-  entropy <- mean(-log(dbinom(y, size = ne, p)))
+  if(entropy_sim){
+    entropy <- mean(-log(dbinom(y, size = ne, p)))
+  } else {
+    entropy <- 0.5 * log2(2*pi*exp(1)*ne*p*(1-p))
+  }
 
   KL_div <- KL_calc(nl, pl, y1, y2, ne, entropy)
 
