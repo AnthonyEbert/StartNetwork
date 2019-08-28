0e407c25b82e67f39430131c51804137479a4c21
Anthony Ebert <anthonyebert@gmail.com>
2019-08-28 14:54:35 -0400

    working with BA model.

--- R/ba_KL.R
+++ R/ba_KL.R
@@ -1,8 +1,11 @@
 
 #' @export
-ba_KL <- function(m, nl = 10, ml = 3, replicates = 1000, entropy_cheat = FALSE, entropy_return = FALSE, ...){
+ba_KL <- function(m, nl = 50, ml = 3, replicates = 1000, entropy_cheat = FALSE, entropy_return = FALSE, ...){
 
-  y <- t(sapply(rep(nl, replicates), FUN = degree_pa_sampler, m = m))
+  g <- igraph::graph(edges = c(1, 2, 1, 3), directed = FALSE)
+
+  y <- t(sapply(rep(nl, replicates), FUN = degree_pa_sampler, m = m, g = g))
+  y <- t(apply(y, 1, sort))
 
   #log_lik <- mean( apply(y, 1, ba_lik2, p = pl) )
 
@@ -15,14 +18,22 @@ ba_KL <- function(m, nl = 10, ml = 3, replicates = 1000, entropy_cheat = FALSE, 
 }
 
 #' @export
-degree_pa_sampler <- function(nl, m){
+degree_pa_sampler <- function(nl, m, g){
 
-  degree(sample_pa(nl, m = m, zero.appeal = 0, directed = FALSE))
+  #output <- rep(0, nl)
+
+  x <- degree(sample_pa(nl, m = m, zero.appeal = 0, directed = FALSE, start.graph = g))
+
+  #output[1:length(x)] <- x
+
+  return(x)
+
+
 }
 
 #' @export
-ba_lik <- function(y, p){
-  (p - 1) * gamma(p) * prod(gamma(y) / gamma(y + p))
+ba_lik <- function(y, m){
+  prod(4 / (y * (y + 1) * (y + 2)))
 }
 
 #' @export
