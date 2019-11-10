8e5cdb5e1a7d7d8fd1047468e4bc087b14993ffb
Anthony Ebert <anthonyebert@gmail.com>
2019-11-10 20:04:03 +0100

    Working. Result gives ~ -74

--- R/ba_dd_KL_m.R
+++ R/ba_dd_KL_m.R
@@ -1,14 +1,42 @@
 
 #' KL Divergence of mechanistic and statistical network models
+#' @param theta_m numeric mechanistic model parameters
+#' @param n integer number of nodes
+#' @param theta_s numeric statistical model parameters
+#' @param replicates
 #' @importFrom magrittr %>%
+#' @examples
+#'
+#' theta_m = 5
+#' theta_s = 1
+#' n = 15
+#'
+#' mech_net = function(m, n, args){args$m = m; args$n = n; do.call(igraph::sample_pa, args)}
+#' mech_args = list(directed = FALSE)
+#' lstat = function(x){sum(igraph::degree(x) == 6)}
+#'
+#' KL_net(
+#'   theta_m = 5,
+#'   theta_s = 1,
+#'   n = 15,
+#'   mech_net = mech_net,
+#'   mech_args = mech_args,
+#'   lstat = lstat
+#' )
+#'
 #' @export
-ba_dd_KL_m <- function(p, n = 10, theta_s = 3, replicates = 1000, sorted = TRUE, lstat = function(x){sum(igraph::degree(x) == 6)} , ...){
+KL_net <- function(theta_m, n = 10, theta_s = 3, replicates = 1000, sorted = TRUE, mech_net, mech_args, lstat){
 
-  n <- choose(nl,2)
+  stopifnot(is.numeric(n))
+  stopifnot(n %% 1 == 0)
+  stopifnot(is.numeric(replicates))
+  stopifnot(replicates %% 1 == 0)
+  stopifnot(replicates > 0)
+  stopifnot(is.logical(sorted))
 
-  g <- igraph::graph(edges = c(1, 2, 1, 3), directed = FALSE)
+  ne <- choose(n,2)
 
-  g <- lapply(rep(nl, replicates), igraph::sample_pa, m = p, directed = FALSE, start.graph = g, ...)
+  g <- lapply(rep(theta_m, replicates), mech_net, n = n, args = mech_args)
   y <- sapply(g, igraph::gsize)
   ds <- lapply(g, igraph::degree)
   if(sorted){
@@ -17,12 +45,12 @@ ba_dd_KL_m <- function(p, n = 10, theta_s = 3, replicates = 1000, sorted = TRUE,
 
   y1 <- aapply(g, lstat) %>% rowMeans()
 
-  stopifnot(length(y1) == length(pl))
+  stopifnot(length(y1) == length(theta_s))
   y2 <- mean(sapply(ds, number_of_graphs_dd, sorted = sorted, type = "log"))
 
   entropy <- entropy_calc(ds, hash = TRUE)
 
-  KL_div <- KL_calc2(nl, pl, y1, y2, ne, entropy)
+  KL_div <- KL_calc2(n, theta_s, y1, y2, ne, entropy)
 
   output <- KL_div - entropy
 
