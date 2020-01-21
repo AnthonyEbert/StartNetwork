46373a57152a854e6732d174e6e23dc37d6c6234
Anthony Ebert <anthonyebert@gmail.com>
2020-01-21 17:27:40 +0100

    edges0

--- script/shiny_data/shiny_data.R
+++ script/shiny_data/shiny_data.R
@@ -8,19 +8,20 @@ n = 15
 replicates = 400
 datapoints = 10
 
-mech_net_altkstars <- purrr::partial(sim_ergm_altkstar, n = !!n)
+mech_net_gnp = purrr::partial(igraph::sample_gnp, n = !!n, ... = , directed = FALSE, loops = FALSE)
 
-theta_s <- 0.5
-theta_p <- rep(seq(-1.5, 1.5, by = 0.1), datapoints)
+true_value <- 0.5
+theta_s <- log(true_value/(1 - true_value))
+theta_p <- rep(seq(0.025, 0.975, by = 0.025), datapoints)
 
 cl <- parallel::makeCluster(parallel::detectCores())
 
-g <- parallel::parLapply(cl, theta_p, StartNetwork::KL_ss, theta_s = theta_s, replicates = replicates, sorted = TRUE, mech_net = mech_net_altkstars, lstat = function(x){as.numeric(ergm::summary_formula(x ~ altkstar(log(10), fixed = TRUE)))}, mirror = TRUE, type = c("Bianconi", "Liebenau"), ergm = TRUE)
+g <- parallel::parLapply(cl, theta_p, StartNetwork::KL_ss, theta_s = theta_s, replicates = replicates, sorted = TRUE, mech_net = mech_net_gnp , lstat = igraph::gsize, mirror = TRUE, type = c("Bianconi", "Liebenau"), ergm = FALSE)
 
 parallel::stopCluster(cl)
 
 df <- StartNetwork::tidy_g(g, tidy = FALSE)
 
-saveRDS(df, "altkstars2.Rds")
+saveRDS(df, "edges0.Rds")
 
 sessionInfo()
