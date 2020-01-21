20bcf7ac6b96c53afa07669d7b9fa27943e6891d
Anthony Ebert <anthonyebert@gmail.com>
2020-01-21 11:21:47 +0100

    altkstars0

---
+++ script/shiny_data/shiny_data.R
@@ -0,0 +1,26 @@
+set.seed(1)
+
+library(StartNetwork)
+library(parallel)
+library(ggplot2)
+
+n = 15
+replicates = 400
+datapoints = 10
+
+mech_net_altkstars <- purrr::partial(sim_ergm_altkstar, n = !!n)
+
+theta_s <- 0.5
+theta_p <- rep(seq(0.1, 1.5, by = 0.1), datapoints)
+
+cl <- parallel::makeCluster(parallel::detectCores())
+
+g <- parallel::parLapply(cl, theta_p, StartNetwork::KL_ss, theta_s = theta_s, replicates = replicates, sorted = TRUE, mech_net = mech_net_altkstars, lstat = function(x){as.numeric(ergm::summary_formula(x ~ altkstar(log(10), fixed = TRUE)))}, mirror = TRUE, type = c("Bianconi", "Liebenau"), ergm = TRUE)
+
+parallel::stopCluster(cl)
+
+df <- StartNetwork::tidy_g(g, tidy = FALSE)
+
+saveRDS(df, "altkstars0.Rds")
+
+sessionInfo()
