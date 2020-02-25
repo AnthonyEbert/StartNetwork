513c729390368b98997d5433d5c236f66ae6b282
AnthonyEbert <anthonyebert@gmail.com>
2020-02-25 17:54:36 +0100

    ergm simulator

---
+++ R/altkstars.R
@@ -0,0 +1,40 @@
+
+#' @export
+kstar <- function(k, dstat){
+  n <- length(dstat) - 1
+  output <- sum(choose(k:n, k) * dstat[k:n])
+  # output <- sapply(k:n, function(i,k){choose(i,k)*dstat[i]}, k = k)
+  output <- sum(output)
+}
+
+#' @export
+altkstars_sn <- function(x, lambda){
+  n <- max(x)
+  dstat <- as.numeric(table(factor(x, levels = 0:n)))
+  dstat <- dstat / sum(dstat)
+  lambda_vec <- (-1/lambda)^(c(0:I(n-3)))
+  output <- sapply(2:I(n-1), kstar, dstat = dstat)
+  output <- output * lambda_vec
+
+  return(sum(output))
+}
+
+#' @export
+gwdegree <- function(x, alpha){
+  n <- max(x)
+  dstat <- as.numeric(table(factor(x, levels = 0:I(n- 1))))
+
+  output <- 0
+
+  for(i in 1:I(n-1)){
+    output <- output + (1 - (1 - exp(-alpha))^i) * dstat[i]
+  }
+
+  output <- output * exp(alpha)
+
+  return(output)
+}
+
+
+
+
---
+++ R/ergm_simulator.R
@@ -0,0 +1,49 @@
+
+#' @export
+ergm_simulator <- function(init, sum_stat, loops, theta, ...){
+
+  stopifnot(class(sum_stat) == "function")
+  stopifnot(loops > 0)
+
+  x = init
+  accepted = matrix(NA, nrow = loops, ncol = length(x))
+  accepted_ss <- rep(NA, loops)
+
+  for(i in 1:loops){
+    s_init <- sum_stat(x, ...)
+    x_star <- qERGM(x)
+    s_star <- sum_stat(x_star, ...)
+
+    acceptance_prob <- number_of_graphs_dd(x_star, mirror = TRUE) - number_of_graphs_dd(x, mirror = TRUE) + s_star %*% theta - s_init %*% theta
+    if(rbinom(1, size = 1, prob = min(1,exp(acceptance_prob))) == 1){
+      x = x_star
+      s_init = s_star
+    }
+    accepted[i,] = x
+    accepted_ss[i] = s_init
+  }
+
+  return(accepted_ss)
+}
+
+#' @export
+qERGM <- function(x){
+  n <- length(x)
+  ind <- sample.int(n, 2)
+
+  if(rbinom(1, 1, 0.5) == 1){
+    x[ind[1]] = x[ind[1]] + 1
+    x[ind[2]] = x[ind[2]] + 1
+  } else {
+    x[ind[1]] = x[ind[1]] - 1
+    x[ind[2]] = x[ind[2]] - 1
+  }
+
+  if(rbinom(1, 1, 0.2) == 1){
+    x = qERGM(x)
+  }
+
+  return(sort(x))
+}
+
+
